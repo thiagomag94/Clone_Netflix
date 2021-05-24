14611d99aa2a42846a5b82e83501cb366c222025
thiagomag94 <thiagomag94@gmail.com>
2021-05-24 01:58:18 -0300

    Atualizando transition no header

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 
 function bigImg(x) {
     
-    x.style.height = "109%";
+    x.style.height = "110%";
     x.style.width = "103%";
     x.style.boxShadow = "6px 6px 6px rgba(0,0,0,0.50)";
+    x.style.transition = " 0.61803399s ease all";
 
     
 }
